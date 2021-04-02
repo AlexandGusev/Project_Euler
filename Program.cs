2e21710c48255cad2559cf2c754c37d6a8330a78
Aleksander Gusev <wanna_be_fly@mail.ru>
2021-04-02 15:58:31 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Eiler_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int a = 0;
+
+            for (int i = 0; i<1000; i++)
+            {
+                if (i%3==0 || i%5==0)
+                {
+                    a += i;
+                }
+            }
+
+            Console.WriteLine(a);
+        }
+    }
+}
